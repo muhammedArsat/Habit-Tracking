d8d6606bd018e9b9ec519139c3b7f48b69e953dd
muhammedArsat <ars21.code@gmail.com>
2025-05-31 13:00:41 +0530

    cors updated v4

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors())
 connectDB();
 app.use(cors({
-  origin: 'https://habit-tracking-frontend.onrender.com/*',
+  origin: 'https://habit-tracking-frontend.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
