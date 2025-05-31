4426ae617d2af2cbe91590e4784fff44056ebb45
muhammedArsat <ars21.code@gmail.com>
2025-05-31 12:42:31 +0530

    cors updated v1

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,16 @@ const connectDB = require('./config/dbConfig');
 const habitRoutes = require('./routes/HabitRoutes');
 const cors =  require('cors')
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 dotenv.config();
 app.use(express.json());
 app.use(cors())
 connectDB();
-// app.use(cors({
-//   origin: 'https://habit-tracking-frontend.onrender.com',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// }));
+app.use(cors({
+  origin: 'https://habit-tracking-frontend.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use('/api/v1', habitRoutes);
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
